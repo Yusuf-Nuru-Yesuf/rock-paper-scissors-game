8b68d2b8bcdadddf4b4308df3e11934b23be0d9b
Yusuf-Nuru-Yesuf <yusufnuru2002@gmail.com>
2024-04-16 15:12:49 +0800

    Implement rock-paper-scissors choice logic

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
 function  getComputerChoice() {
     let randomNumber = Math.floor(Math.random() * 3 + 1);
+    if (randomNumber === 1 ){
+        return "Rock" ;
+    }
+    else if (randomNumber === 2){
+        return "Paper" ;
+    }
+    else{
+        return "Scissors" ;
+    }
 }

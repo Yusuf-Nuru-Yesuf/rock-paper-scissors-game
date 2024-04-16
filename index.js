8d8aa1130c2923540634b7a513bae4ac2912e700
Yusuf-Nuru-Yesuf <yusufnuru2002@gmail.com>
2024-04-16 15:58:40 +0800

    Add the rock-paper-scissors game logic

--- index.js
+++ index.js
@@ -17,5 +17,22 @@ function getUserChoice() {
 }
 
 function playRound(userSelection, computerSelection) {
+    
+    if (userSelection !== "Rock" && userSelection !== "Paper" && userSelection !== "Scissors" ) {
+        return "Invalid user selection. Please choose 'rock', 'paper', or 'scissors'.";
+    }
 
+    if(userSelection === computerSelection ) {
+        return "It's a tie!"
+    }  
+    else if (
+        (userSelection === "Rock" && computerSelection === "Paper") ||
+        (userSelection === "Paper" && computerSelection === "Scissors") ||
+        (userSelection === "Scissors" && computerSelection === "Rock")
+   ) {
+        return `You LOSE! ${computerSelection} beats ${userSelection}` ;
+   }
+   else {
+        return `You WIN! ${userSelection} beats ${computerSelection}` ;
+   }
 }
